5d3583020070fc5fa8d81ac63c119a0f45a72e7c
AbdulrahmanIbraheem <shaweeshabdulrahmahn@gmail.com>
2024-12-13 00:17:22 +0800

    added cinema model

---
+++ eTickets/Models/Entities/CinemaModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models.Entities
+{
+    public class CinemaModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public String Logo = String.Empty;
+
+        public string Name = String.Empty;  
+
+        public string Description = String.Empty;
+    }
+}
---
+++ eTickets/Models/Entities/Producer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models.Entities
+{
+    public class Producer
+    {
+        [Key    ]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public string Image { get; set; } = String.Empty;
+
+        public string Biography { get; set; } = String.Empty;
+    }
+}
